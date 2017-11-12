e43f1543dff6ebbbac2eee1a2e1ce53e057c0f04
vanstorm9 <antlowhur@yahoo.com>
2017-11-11 22:02:56 -0500

    Finished walking / standing recognizer

--- Arduino/UnityCode/Assets/communication.cs
+++ Arduino/UnityCode/Assets/communication.cs
@@ -14,9 +14,54 @@ public class communication : MonoBehaviour
 
     void Update()
     {
+
+
         try
         {
-            print(sp.ReadLine());
+            //print(sp.ReadLine());
+            string sentence = sp.ReadLine();
+
+            //string[] splitArray = sentence.Split((new string[] , char.Parse("/"));
+            string[] arr = sentence.Split('/');
+
+            /*
+            print(arr[0]);
+            print(arr[1]);
+            print(arr[2]);
+            print("----------");
+            */
+
+            int XVal = int.Parse(arr[0]);
+            int YVal = int.Parse(arr[1]);
+            int ZVal = int.Parse(arr[2]);
+
+            int lowerLimit = 260;
+            int upperLimit = 270;
+
+            int fluctRange = 7;
+            bool walking = false;
+
+            // 
+            if ((lowerLimit - fluctRange <= XVal && XVal <= upperLimit + fluctRange) &&
+                 (lowerLimit - fluctRange <= YVal && YVal <= upperLimit + fluctRange) &&
+                 (lowerLimit - fluctRange <= ZVal && ZVal <= upperLimit + fluctRange))
+            {
+                // standing
+                print("Standing");
+                walking = false;
+            }
+            else {
+                print("Walking");
+                walking = true;
+
+                /*
+                print(XVal);
+                print(YVal);
+                print(ZVal);
+                print("----------");
+                */
+            }
+
         }
         catch (System.Exception)
         {
