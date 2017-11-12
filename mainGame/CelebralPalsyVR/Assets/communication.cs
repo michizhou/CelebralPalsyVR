2374b6db6e68b14cba901b07989239d639797c6d
vanstorm9 <antlowhur@yahoo.com>
2017-11-12 04:33:46 -0500

    Managed to get movement script to work

---
+++ mainGame/CelebralPalsyVR/Assets/communication.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class communication : MonoBehaviour
+{
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    void Update()
+    {
+
+
+        try
+        {
+            //print(sp.ReadLine());
+            string sentence = sp.ReadLine();
+
+            //string[] splitArray = sentence.Split((new string[] , char.Parse("/"));
+            string[] arr = sentence.Split('/');
+
+            /*
+            print(arr[0]);
+            print(arr[1]);
+            print(arr[2]);
+            print("----------");
+            */
+
+            int XVal = int.Parse(arr[0]);
+            int YVal = int.Parse(arr[1]);
+            int ZVal = int.Parse(arr[2]);
+
+            int lowerLimit = 250;
+            int upperLimit = 260;
+
+            int fluctRange = 7;
+            bool walking = false;
+
+    
+            if ((lowerLimit - fluctRange <= XVal && XVal <= upperLimit + fluctRange) &&
+                 (lowerLimit - fluctRange <= YVal && YVal <= upperLimit + fluctRange) &&
+                 (lowerLimit - fluctRange <= ZVal && ZVal <= upperLimit + fluctRange))
+            {
+                // standing
+                print("Standing");
+                walking = false;
+            }
+            else {
+                // walking
+                print("Walking");
+                walking = true;
+                transform.position += transform.forward *4;
+                /*
+                print(XVal);
+                print(YVal);
+                print(ZVal);
+                print("----------");
+                */
+            }
+
+        }
+        catch (System.Exception)
+        {
+        }
+    }
+}
